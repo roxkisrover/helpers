5dd1b06937dd1a7c6567e9eb6b7b7767387b285b
Anton Nikolaenko <roxkisrover@icloud.com>
2022-01-11 17:45:16 +0500

    Create deepCompare.js

---
+++ deepCompare.js
@@ -0,0 +1,8 @@
+export const deepCompare = (item1, item2) => {
+    if (!!item1 && typeof item1 === 'object' && !!item2 && typeof item2 == 'object') {
+        return Object.keys(item1).length === Object.keys(item2).length 
+            && Object.keys(item1).every(key => deepCompare(item1[key], item2[key]));  
+    }
+  
+    return o1 === o2;
+};
