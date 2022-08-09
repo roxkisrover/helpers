4ef19ccb7c96edf62ac32a9a5664362e77d10e1b
Anton Nikolaenko <roxkisrover@icloud.com>
2022-08-09 11:15:24 +0500

    Update isValueEmpty.js

--- isValueEmpty.js
+++ isValueEmpty.js
@@ -1,20 +1,25 @@
 export const isValueEmpty = (value) => {
-    if (typeof value === 'number' || typeof value === 'boolean') {return false;}
+  if (typeof value === 'number' || typeof value === 'boolean') {
+    return false;
+  }
 
-    if (typeof value === 'undefined' || value === null) {return true;}
+  if (typeof value === 'undefined' || value === null) {
+    return true;
+  }
 
-    if (typeof value.length !== 'undefined') {
-        if ((/^[\s]*$/).test(value.toString())) {return true;}
-        return value.length === 0;
+  if (typeof value.length !== 'undefined') {
+    if ((/^[\s]*$/).test(value.toString())) {
+      return true;
     }
 
-    let count = 0;
+    return value.length === 0;
+  }
 
-    Object.keys(value).forEach((item) => {
-        if (value.hasOwnProperty(item)) {
-            count += 1;
-        }
-    });
+  const ownPropertiesCount = Object.keys(value).reduce((acc, curr) => (
+    Object.prototype.hasOwnProperty.call(value, curr)
+      ? acc + 1
+      : acc
+  ), 0);
 
-    return count === 0;
+  return ownPropertiesCount === 0;
 };
